83f526902ff0f8eed93f87a9534e2a46c3b816f1
Maksym Serhiyevich Kushnirenko <100068913+Keernex@users.noreply.github.com>
2023-07-19 23:52:21 +0300

    no done

--- main.cpp
+++ main.cpp
@@ -27,52 +27,52 @@ void main()
 	//		break;
 	//	}
 	//} while (true);
-	FileShops file_shops;
-	Shops shops_funk;
-	vector<Shops> shops;
-	int count = 10;
-	file_shops.load_shop(shops,count);
-	do
-	{
-		int q = 0;
-		cout << "Enter number: ";
-		cin >> q;
-		if (q == 1)
-		{
-			for (int i = 0; i < count; i++)
-			{
-				shops[i].input_shop(i);
-			}
-			file_shops.save_shop(shops, count);
-		}
-		if (q == 2)
-		{
-			int index;
-			cout << "Enter index: ";
-			cin >> index;
-			shops = shops_funk.redaction_shop_index(index, shops);
-			file_shops.save_shop(shops, count);
-		}
-		if (q == 3)
-		{
-			for (int i = 0; i < count; i++)
-			{
-				shops[i].print_shop(i);
-			}
-		}
-		if (q == 4)
-		{
-			shops_funk.search_shop_name(shops);
-		}
-		if (q == 5)
-		{
-			shops = shops_funk.remove_shop(shops);
-		}
-		if (q == 6)
-		{
-			break;
-		}
-	} while (true);
+	//FileShops file_shops;
+	//Shops shops_funk;
+	//vector<Shops> shops;
+	//int count = 10;
+	//file_shops.load_shop(shops,count);
+	//do
+	//{
+	//	int q = 0;
+	//	cout << "Enter number: ";
+	//	cin >> q;
+	//	if (q == 1)
+	//	{
+	//		for (int i = 0; i < count; i++)
+	//		{
+	//			shops[i].input_shop(i);
+	//		}
+	//		file_shops.save_shop(shops, count);
+	//	}
+	//	if (q == 2)
+	//	{
+	//		int index;
+	//		cout << "Enter index: ";
+	//		cin >> index;
+	//		shops = shops_funk.redaction_shop_index(index, shops);
+	//		file_shops.save_shop(shops, count);
+	//	}
+	//	if (q == 3)
+	//	{
+	//		for (int i = 0; i < count; i++)
+	//		{
+	//			shops[i].print_shop(i);
+	//		}
+	//	}
+	//	if (q == 4)
+	//	{
+	//		shops_funk.search_shop_name(shops);
+	//	}
+	//	if (q == 5)
+	//	{
+	//		shops = shops_funk.remove_shop(shops);
+	//	}
+	//	if (q == 6)
+	//	{
+	//		break;
+	//	}
+	//} while (true);
 }
 
 
